0265189c417f6f3b4de604f0e4ad3f8c8a58c6c3
Eric Li <ericli@knights.ucf.edu>
2021-05-19 16:49:51 -0600

    Add text to medusa.js

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,25 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+  constructor(name) {
+    this.name = name;
+    this.statues = [];
+  }
+  gazeAtVictim(person) {
+    var victim = new Statue(person.name);
+    if (this.statues.length < 3) {
+      this.statues.push(victim);
+    } else {
+      var freedPerson = new Person(this.statues[0].name, "relieved");
+      this.statues.shift();
+      this.statues.push(victim);
+      return freedPerson;
+    }
+  }
+}
+
+
+
+
+module.exports = Medusa;
