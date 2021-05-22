2b63872b4900d6569f60c129dea2bdb2c7a783da
Eric Li <ericli@knights.ucf.edu>
2021-05-22 09:09:53 -0600

    Add text to werewolf.js

---
+++ mythical-creatures/exercises/werewolf.js
@@ -0,0 +1,36 @@
+class Werewolf {
+  constructor(name) {
+    this.name = name;
+    this.form = "human";
+    this.hungry = false;
+  }
+
+  completeTransformation() {
+    if (this.form === "human") {
+      this.form = "wolf";
+      this.hungry = true;
+      return "Aaa-Woooo!";
+    } else {
+      this.form = "human";
+      this.hungry = false;
+      return "Where are I?";
+    }
+  }
+
+  eatVictim(victim) {
+    if (this.hungry === true && this.form === "wolf") {
+      victim.alive = false;
+      this.form = "human";
+      this.hungry = false;
+      return `Yum, ${victim.name} was delicious.`
+    } else {
+      return `No way am I eating ${victim.name}, I'd like a burger!`;
+    }
+  }
+}
+
+
+
+var Victim = require('./victim');
+
+module.exports = Werewolf;
