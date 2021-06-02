f95a05e095ddf5b9cc5a898b15e1fa8c7ea5bfbc
Eric Li <ericli@knights.ucf.edu>
2021-06-02 11:34:06 -0600

    Add text to human.js

---
+++ mythical-creatures/exercises/human.js
@@ -0,0 +1,17 @@
+class Human {
+  constructor(name) {
+    this.name = name;
+    this.encounterCounter = 0;
+    this.knockedOut = false;
+  }
+
+  noticesOgre(ogre) {
+    if (this.encounterCounter % 3 === 0 && this.encounterCounter != 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+module.exports = Human;
