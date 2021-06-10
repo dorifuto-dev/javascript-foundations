fc9905143b039c6a9c974b04c85ae43939129de7
Eric Li <ericli@knights.ucf.edu>
2021-06-09 21:10:36 -0600

    Add text to craft.js

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,24 @@
-class Craft {
+var total = 0;
 
+class Craft {
+  constructor(object) {
+    this.name = object.type;
+    this.materials = object.materials;
+    this.completed = false;
+  }
+
+  completeCraft() {
+    this.completed = true;
+    return "All done! It looks great!";
+  }
+
+  calculateProjectTotal() {
+    for (var i = 0; i < this.materials.length; i++) {
+      total += this.materials[i].calculateMaterialCost()
+    }
+    return total;
+  }
 }
+
 
 module.exports = Craft;
