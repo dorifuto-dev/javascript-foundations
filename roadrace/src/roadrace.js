2d263e60cbd6a27afb919a5d3b6b697552d4442c
Eric Li <ericli@knights.ucf.edu>
2021-06-09 21:34:17 -0600

    Add text to roadrace.js

--- roadrace/src/roadrace.js
+++ roadrace/src/roadrace.js
@@ -1,5 +1,26 @@
 class Roadrace {
+  constructor(venue) {
+    this.name = venue.title,
+    this.location = venue.city,
+    this.distance = 0,
+    this.participants = []
+  }
 
+  setDistance(distance) {
+    this.distance = distance;
+    return `The ${this.name} in ${this.location} is a ${distance} mile race.`;
+  }
+
+  registerParticipants(runner) {
+    this.participants.push(runner);
+  }
+
+  completeRace() {
+    for (var i = 0; i < this.participants.length; i++) {
+      this.participants[i].completedRaces.push(this.name);
+      this.participants[i].runSomeMiles(this.distance);
+    }
+  }
 }
 
 module.exports = Roadrace;
