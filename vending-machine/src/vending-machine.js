85833998429b6740f4c8706ad2bf97595e81cfdb
Eric Li <ericli@knights.ucf.edu>
2022-01-17 17:45:44 -0500

    Write code to pass vending machine test

--- vending-machine/src/vending-machine.js
+++ vending-machine/src/vending-machine.js
@@ -1,5 +1,30 @@
 class VendingMachine {
+  constructor(machine) {
+    this.id = machine.id;
+    this.isBroken = machine.isBroken;
+    this.snacks = [];
+  }
 
+  addSnacks(snack) {
+    if (this.snacks.some(item => item.name === snack.name)) {
+      return "Sorry, that snack is already stocked! Try adding a different snack.";
+    } else {
+      this.snacks.push(snack);
+    }
+  }
+
+  purchaseSnack(name, dollars) {
+
+    for (var i = 0; i < this.snacks.length; i++) {
+      var change = dollars - this.snacks[i].price;
+      if (this.snacks[i].name === name && this.snacks[i].itemsInStock > 0) {
+        this.snacks[i].itemsInStock--;
+        return `Success! Here is $${change} back!`
+      } else {
+        return "Sorry, no items in stock. Try another item.";
+      }
+    }
+  }
 }
 
 module.exports = VendingMachine;
