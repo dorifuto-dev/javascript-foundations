11bebeda51b3415ceff735c38d3e17c4f5771034
Eric Li <ericli@knights.ucf.edu>
2022-01-17 17:46:03 -0500

    Pass part test

--- oregon-trail/src/part.js
+++ oregon-trail/src/part.js
@@ -1,5 +1,22 @@
 class Part {
+  constructor(type) {
+    this.type = type;
+    this.broken = false;
+    this.repaired = false;
+  }
 
+  break() {
+    this.broken = true;
+  }
+
+  repair() {
+    if (!this.repaired) {
+      this.broken = false;
+      this.repaired = true;
+    } else {
+      return `This ${this.type} has seen better days, we\'ll need a brand new one!`
+    } 
+  }
 }
 
 module.exports = Part;
