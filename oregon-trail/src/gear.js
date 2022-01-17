70c7f42311e1e484428370c48ce444278b20ced4
Eric Li <ericli@knights.ucf.edu>
2022-01-17 17:45:53 -0500

    Pass gear test

--- oregon-trail/src/gear.js
+++ oregon-trail/src/gear.js
@@ -1,5 +1,16 @@
 class Gear {
+  constructor(type, number) {
+    this.type = type;
+    this.quantity = number;
+  }
 
+  checkForValidity() {
+    if (this.type === 'ammunition' || this.type === 'food' || this.type === 'clothes') {
+      return `Great, we\'ll need lots of ${this.type}!`
+    } else {
+      return `I don\`t think a ${this.type} will help us.`
+    }
+  }
 }
 
 module.exports = Gear;
